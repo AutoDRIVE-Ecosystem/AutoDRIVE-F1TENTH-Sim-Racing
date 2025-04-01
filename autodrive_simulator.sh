6dcb3a2f1d0b75f8a7eeecf335d58c1e0c368c3c
Tinker Twins <tinkertwinsofficial@gmail.com>
2025-04-01 19:35:00 -0400

    Update autodrive_simulator.sh

--- autodrive_simulator.sh
+++ autodrive_simulator.sh
@@ -8,4 +8,7 @@ cd /home/autodrive_simulator
 # ./AutoDRIVE\ Simulator.x86_64
 
 # Launch AutoDRIVE Simulator Headless
+# xvfb-run ./AutoDRIVE\ Simulator.x86_64 -ip 127.0.0.1 -port 4567
+
+# Launch AutoDRIVE Simulator without Graphics
 # ./AutoDRIVE\ Simulator.x86_64 -batchmode -nographics -ip 127.0.0.1 -port 4567
